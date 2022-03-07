8f5f4ed98a8d14b7690266baa66cf12c06a097f5
Ruka257 <r.akingbala52@gmail.com>
2022-03-07 14:46:06 +0000

    QBAFA-13 begun home page and view (read) all routes. db tbc

--- application/routes.py
+++ application/routes.py
@@ -1,4 +1,17 @@
 from flask import FLASK, render_template, url_for #htmls templates for routes, url_for crud index hyperlinks
 from application import app, db
 from application.models import Tasks
-from application.forms import CreateForm, UpdateForm
+# from application.forms import CreateForm, UpdateForm #possibly sprint 2?
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'Welcome to the Festival Planner'
+
+@app.route('/viewall')
+def viewall():
+    all_festivals = Festivals.query.all()
+    festival_string = ""
+    for festival in all_festivals:
+        task_string += "<br>"+ festival.name
+    return festival_string
