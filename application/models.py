17aa7014689976ba4f6a8770f0b78704cce398be
Ruka257 <r.akingbala52@gmail.com>
2022-03-09 17:03:45 +0000

    QBAFA-17 shortened model id columns and changed FK

--- application/models.py
+++ application/models.py
@@ -4,15 +4,15 @@ from application import db
 from datetime import datetime, date
 
 class Festivals(db.Model): #schema
-    festival_id = db.Column(db.Integer, unique=True, primary_key=True)
+    id = db.Column(db.Integer, unique=True, primary_key=True)
     festival_name = db.Column(db.__str__(50), nullable=False)
     start_date = db.Column(db.date)
     location = db.Column(db.__str__(100), nullable=False)
     attended = db.Column(db.Boolean, default=False)
 
 class Set_Times(db.Model): #schema
-    set_time_id = db.Column(db.Integer, unique=True, primary_key=True)
+    id = db.Column(db.Integer, unique=True, primary_key=True)
     set_time = db.Column(db.datetime)
-    festival_id = db.Column(db.Integer, unique=True, ForeignKey=True) #FK calling from...?
+    festival_id = db.Column(db.Integer, db.ForeignKey('festivals.id'), nullable=False) #trying new FK Syntax
     act_name = db.Column(db.__str__(100), nullable=False)
     seen = db.Column(db.Boolean, default=False)
