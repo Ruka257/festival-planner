355c62255046af99926c3f43f9e04f1e1f8bbc0e
Ubuntu <azureuser@rebekahakingbala-jenkins.dq2pivvzhwverptj4qj50lbwdh.zx.internal.cloudapp.net>
2022-03-09 14:19:19 +0000

    QBAFA-17 models match ERD but searching for correct FK syntax for relaationship modelling!

--- application/models.py
+++ application/models.py
@@ -1,17 +1,18 @@
 from enum import unique
 from sqlalchemy import ForeignKey, ForeignKeyConstraint, UniqueConstraint, null
 from application import db
+from datetime import datetime, date
 
 class Festivals(db.Model): #schema
     festival_id = db.Column(db.Integer, unique=True, primary_key=True)
-    festival_name = db.Column(db.__str__,)
+    festival_name = db.Column(db.__str__(50), nullable=False)
     start_date = db.Column(db.date)
-    location = db.Column(db.__str__)
+    location = db.Column(db.__str__(100), nullable=False)
     attended = db.Column(db.Boolean, default=False)
 
 class Set_Times(db.Model): #schema
     set_time_id = db.Column(db.Integer, unique=True, primary_key=True)
     set_time = db.Column(db.datetime)
-    festival_id = db.Column(db.Integer, unique=True, ForeignKey=True)
-    act_name = db.Column(db.__str__)
+    festival_id = db.Column(db.Integer, unique=True, ForeignKey=True) #FK calling from...?
+    act_name = db.Column(db.__str__(100), nullable=False)
     seen = db.Column(db.Boolean, default=False)
