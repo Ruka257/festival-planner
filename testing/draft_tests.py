7583b6c9727c7e2c2dfdc3eb7ab9caa8db334a80
Ruka257 <r.akingbala52@gmail.com>
2022-03-10 14:19:37 +0000

    QBAFA-4 create  unit testing script in draft_tests py

--- testing/draft_tests.py
+++ testing/draft_tests.py
@@ -1,4 +1,16 @@
 from flask import url_for
 from flask_testing import TestCase
+from os import getenv
+import uuid
 
 from application import app, db, Festivals, Set_Times
+
+#base class for test creation
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(SQLALCHEMY_DATABASE_URI=getenv('DATABASE_URI'), #test sqlite
+        SECRET_KEY=str(uuid.uuid4()), #randomly generated secret key,
+        debug=True,
+        WTF_CSRF_ENABLED=False, #in production set to true
+        )
+        return app
